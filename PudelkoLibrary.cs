cba870538aad48f47edadb9752bed87ab63b2f98
Agnieszka_KK <agnieszka.korczakkus@gmail.com>
2022-04-27 00:19:55 +0200

    Commit3

--- PudelkoLibrary.cs
+++ PudelkoLibrary.cs
@@ -1,35 +1,126 @@
-﻿namespace PudelkoLibrary
+﻿using System;
+
+namespace PudelkoLibrary
 {
     class Pudelko
     {
         private double a;
         private double b;
         private double c;
+        private UnitOfMeasure unit;
 
-        public double A => a;
-        public double B => b;
-        public double C => c;
+        public double A => calculate_To_Scale(a);
+        /* public double A
+          { 
+              get{ return calculate_To_Scale(a);}
+              
 
-       /* public Pudelko(double a, double b, double c)
+          public double B
+          { 
+              get{ return b;}
+             } 
+              
+        public double C
+        { 
+            get{ return c;}
+            
+            }
+
+         */
+
+        public double B => calculate_To_Scale(b);
+        public double C => calculate_To_Scale(c);
+
+       
+
+        public Pudelko(double a=double.NaN , double b= double.NaN, double c= double.NaN, UnitOfMeasure unit= UnitOfMeasure.meter)
         {
+            this.unit = unit;
+            if(Double.IsNaN(a))
+            {
+                this.a = 100;
+            }
+            else 
+            {
+                double _a = calculate_To_Milimeters(a);
 
+                if (_a > 0 && _a <= 10000) { this.a = _a; }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            if (Double.IsNaN(b))
+            {
+                this.b = 100;
+            }
+            else
+            {
+                double _b = calculate_To_Milimeters(b);
+
+                if (_b > 0 && _b <= 10000) { this.b = _b; }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            if (Double.IsNaN(c))
+            {
+                this.c = 100;
+            }
+            else
+            {
+                double _c = calculate_To_Milimeters(c);
+
+                if (_c > 0 && _c <= 10000) { this.c = _c; }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
         }
 
-        public Pudelko()
+        private double calculate_To_Milimeters(double value)
         {
-
-        }*/
-        public Pudelko(double a=10, double b=10, double c=10, UnitOfMeasure unit= UnitOfMeasure.meter)
-        {
-
+            if(UnitOfMeasure.meter.Equals(this.unit))
+            {
+                return value * 1000;
+            }
+            else if(UnitOfMeasure.centimeter.Equals(this.unit))
+            {
+                return value * 10;
+            }
+            else 
+            {
+                return value;
+            }
         }
-
+        private double calculate_To_Scale(double value)
+        {
+            if(UnitOfMeasure.meter.Equals(this.unit))
+            {
+                return value/1000;
+            }
+            else if(UnitOfMeasure.centimeter.Equals(this.unit))
+            {
+                return value/10;
+            }
+            else
+            {
+                return value;
+            }
+        }
         public override string ToString()
         {
             return base.ToString();
+            //Console.WriteLine($"{a} {unit} \u00D7 {b} {unit} \u00D7 {c} {unit}")
         }
-        public string ToString(string Format = "")
+        public string ToString(string Format = "{a} {unit} \u00D7 {b} {unit} \u00D7 {c} {unit}")
         {
+            /*if(UnitOfMeasure=meter)
+            { String.Format("{0:#,#.000}", a)}*/
             return base.ToString();
         }
 
